6899bc0a7fb65351108ed42a86139bd75a7be076
valeriich <iixmcmlxxiii@gmail.com>
2021-10-26 18:43:25 +0000

    Midterm project

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from flask import Flask, request, jsonify, render_template
 
 app=Flask(__name__)
 
-@app.route('/index/')
+@app.route('/')
 def index():
     #return "<p>Hello, World!</p>"
-    return render_template('index.html')
+    return render_template('main.html')
 
 
 if __name__=="__main__":
