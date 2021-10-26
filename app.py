eb2e5b9451fd632e16ed050695d8547c07e442c8
valeriich <iixmcmlxxiii@gmail.com>
2021-10-26 20:32:19 +0000

    Midterm project

--- app.py
+++ app.py
@@ -5,11 +5,18 @@ from flask import Flask, request, jsonify, render_template
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def main():
     #return "<p>Hello, World!</p>"
     return render_template('main.html')
 
+@app.route("/predict", methods=['POST'])
+def predict():
+
+    if request.method == 'POST':
+        s = int(request.form['season'])
+    
+    return render_template('index.html', prediction_text="You Can Sell The Car at {}".format(s))
 
 if __name__=="__main__":
     app.run()
