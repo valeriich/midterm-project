5cb73d8ac099e6c89939685b90b5d548a23af878
valeriich <iixmcmlxxiii@gmail.com>
2021-10-27 14:50:46 +0000

    Midterm project

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import lightgbm
 from flask import Flask, request, jsonify, render_template
 import pickle
+from utils import *
 
 
 
@@ -28,59 +29,14 @@ def predict():
     # save data to dictionary
     X_dict = {keys[i]: values[i] for i in range(len(keys))}
     
-    # normalize weather data
-    X_dict['temp'] += 8
-    X_dict['temp'] /= (39 + 8)
-    X_dict['windspeed'] /= 67
-    X_dict['hum'] /= 100
-    
-    # creating 'day_type' feature
-    ### 2 - working day
-    ### 1 - weekend
-    ### 0 - holiday
-    if X_dict['holiday'] == 1:
-        X_dict['day_type'] = 0
-    elif (X_dict['weekday'] == 6) or (X_dict['weekday'] == 0):
-        X_dict['day_type'] = 1
-    else:
-        X_dict['day_type'] = 2
-    # don't need 'holiday' feature anymore
-    del X_dict['holiday']
-    
-    # binning hour into 'RegisteredHourBins' feature
-    bins = np.array([1.5, 5.5, 6.5, 8.5, 16.5, 18.5, 20.5, 22.5])
-    labels = np.arange(len(bins)-1)
-    label = pd.cut([X_dict['hr']], bins=bins, labels=labels).fillna(0).astype(int)[0]
-    remap_labels = {0: 0, 1: 1, 2: 5, 3: 3, 4: 6, 5: 4, 6: 2}
-    X_dict['RegisteredHourBins'] = remap_labels[label]
-    
-    # binning hour into 'CasualHourBins' feature
-    bins = np.array([7.5, 8.5, 10.5, 17.5, 19.5, 21.5])
-    labels = np.arange(len(bins)-1)
-    label = pd.cut([X_dict['hr']], bins=bins, labels=labels).fillna(0).astype(int)[0]
-    remap_labels = {0: 0, 1: 2, 2: 4, 3: 3, 4: 1}
-    X_dict['CasualHourBins'] = remap_labels[label]
-    
-    # predicting future, so year is 1
-    X_dict['yr'] = 1
-    
-    features1 = ['temp', 'hum', 'windspeed', 'hr', '3_days_sum_casual',
-            'rolling_mean_12_hours_casual','season', 'yr', 'mnth',  
-            'day_type', 'weathersit', 'CasualHourBins', 'weekday']
-
-    features2 = ['temp', 'hum', 'windspeed', 'hr', '3_days_sum_registered',
-            'rolling_mean_12_hours_registered', 'season', 'yr', 'mnth',  
-            'day_type', 'weathersit', 'RegisteredHourBins', 'weekday']
-    
-    X_1 = np.array([X_dict[feature] for feature in features1]).reshape(1, -1)
-    X_2 = np.array([X_dict[feature] for feature in features2]).reshape(1, -1)
+    X_1, X_2 = prepare_data(X_dict)
     
     casual = int(np.around(model_1.predict(X_1).clip(0), 0)[0])
     registered = int(np.around(model_2.predict(X_2).clip(0), 0)[0])
     
     total = casual + registered
     
-    return render_template('main.html', prediction_text="Predicted demand: {} bikes".format(total))
+    return render_template('main.html', prediction_text="Predicted demand for bicycles at this hour of the day: {} bikes".format(total))
 
 if __name__=="__main__":
     app.run()
