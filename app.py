0540889813496a75cd559095025d92de980cc8f1
valeriich <iixmcmlxxiii@gmail.com>
2021-10-27 21:12:27 +0000

    Midterm project

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def predict():
 
     # visualize the first prediction's explanation
     shap.plots.waterfall(shap_values[0], max_display=14, show=False)
-    plt.savefig(plot1, format = "png",dpi = 150, bbox_inches = 'tight')
+    plt.figure.savefig(plot1, format = "png",dpi = 150, bbox_inches = 'tight')
     
     # explain the model's predictions using SHAP
     explainer = shap.Explainer(model_2, feature_names=features2)
@@ -98,7 +98,7 @@ def predict():
 
     # visualize the first prediction's explanation
     shap.plots.waterfall(shap_values[0], max_display=14, show=False)
-    plt.savefig(plot2, format = "png",dpi = 150, bbox_inches = 'tight')
+    plt.figure.savefig(plot2, format = "png",dpi = 150, bbox_inches = 'tight')
     
     
     return render_template('main.html', prediction_text="Predicted demand for bicycles at this hour of the day: {} bikes".format(total), url_plot1=plot1)
