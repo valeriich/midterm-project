48c24ca0619502e0ded8080eec4c096c4b9ad648
valeriich <iixmcmlxxiii@gmail.com>
2021-10-27 08:32:37 +0000

    Midterm project

--- app.py
+++ app.py
@@ -12,10 +12,12 @@ def main():
 
 @app.route("/predict", methods=['POST'])
 def predict():
-
-    input_values = [float(i) for i in request.form.values()]
+    # get data from html page inputs
+    values = [i for i in request.form.values()]
+    keys = [i for i in request.form.keys()]
+    X_dict = {keys[i]: values[i] for i in range(len(keys))}
         
-    return render_template('main.html', prediction_text="Predicted demand:\ncasual users {}\nregistered users {}".format(input_values[0], input_values[1]))
+    return render_template('main.html', prediction_text="Predicted demand:\ncasual users {}\nregistered users {}".format(X_dict['season'], X_dict['rolling_mean_12_hours_casual']))
 
 if __name__=="__main__":
     app.run()
