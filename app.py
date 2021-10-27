394f699cffc68035977dc666bc3c828328bfc204
valeriich <iixmcmlxxiii@gmail.com>
2021-10-27 09:05:31 +0000

    Midterm project

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-
+#import numpy as np
+import pandas as pd
+import pickle
 from flask import Flask, request, jsonify, render_template
 
 
+with open('model_1.pkl', 'rb') as f:
+    model_1 = pickle.load(f)
+
+with open('model_2.pkl', 'rb') as f:
+    model_2 = pickle.load(f)
+
+
+# function to normalize temperature
+def get_norm_temp(temp):
+    return (temp + 8) / (39 + 8)
 
 app=Flask(__name__)
 
@@ -12,12 +24,45 @@ def main():
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    # get data from html page inputs
+    
+    # get data from html page input fields
     values = [i for i in request.form.values()]
     keys = [i for i in request.form.keys()]
+    # save data to dictionary
     X_dict = {keys[i]: values[i] for i in range(len(keys))}
-        
-    return render_template('main.html', prediction_text="Predicted demand:\ncasual users {}\nregistered users {}".format(X_dict['season'], X_dict['rolling_mean_12_hours_casual']))
+    
+    # normalize weather data
+    X_dict['temp'] = get_norm_temp(X_dict['temp'])
+    X_dict['windspeed'] = X_dict['windspeed'] / 67
+    X_dict['hum'] = X_dict['hum'] / 100
+    
+    # creating 'day_type' feature
+    ### 2 - working day
+    ### 1 - weekend
+    ### 0 - holiday
+    if X_dict['holiday'] == 1:
+        X_dict['day_type'] = 0
+    elif (X_dict['weekday'] == 6) | (X_dict['weekday'] == 0):
+        X_dict['day_type'] = 1
+    else:
+        X_dict['day_type'] = 2
+    # don't need 'holiday' feature anymore
+    del X_dict['holiday']
+    
+    # binning hour into 'RegisteredHourBins' feature
+    bins = np.array([1.5, 5.5, 6.5, 8.5, 16.5, 18.5, 20.5, 22.5])
+    labels = np.arange(len(bins)-1)
+    X_dict['RegisteredHourBins'] = pd.cut([X_dict['hr']], bins=bins, labels=labels).fillna(0).astype(int)[0]
+    
+    # binning hour into 'CasualHourBins' feature
+    bins = np.array([7.5, 8.5, 10.5, 17.5, 19.5, 21.5])
+    labels = np.arange(len(bins)-1)
+    X_dict['CasualHourBins'] = pd.cut([X_dict['hr']], bins=bins, labels=labels).fillna(0).astype(int)[0]
+    
+    
+    
+    
+    return render_template('main.html', prediction_text="Predicted demand:\ncasual users {}\nregistered users {}".format(X_dict['CasualHourBins'], X_dict['RegisteredHourBins']))
 
 if __name__=="__main__":
     app.run()
